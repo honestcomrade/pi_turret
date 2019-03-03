3b8b54b96790ad20d212bd5cc49eda4be6fddb58
Joe Saraceno <honestcomrade@gmail.com>
2019-03-02 19:58:04 -0800

    test directions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from Stepper import Stepper
+from time import sleep
 
 EN_PIN  = 23
 STP_PIN = 27
@@ -10,6 +11,9 @@ stepper = Stepper(23, 27, 22)
 def main():
   print("Setting Up...")
   stepper.step('left', 1)
+  sleep(1)
+  stepper.step('right', 2)
+
   print("Ran...")
 
 
